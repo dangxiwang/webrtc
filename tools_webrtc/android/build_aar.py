616e8fb8cac1c9a779fda5a7aea8eff8c7524702
Yura Yaroshevich <yura.yaroshevich@gmail.com>
2018-05-24 12:52:14 +0300

    Fully regard --verbose flag in build_aar.py
    Bug: None
    Change-Id: I09d18a57b3df4e698784fb7d58c02e8adecddefa

--- tools_webrtc/android/build_aar.py
+++ tools_webrtc/android/build_aar.py
@@ -129,7 +129,7 @@ def _GetArmVersion(arch):
     raise Exception('Unknown arch: ' + arch)
 
 
-def Build(build_dir, arch, use_goma, extra_gn_args):
+def Build(build_dir, arch, use_goma, extra_gn_args, verbose):
   """Generates target architecture using GN and builds it using ninja."""
   logging.info('Building: %s', arch)
   output_directory = _GetOutputDirectory(build_dir, arch)
@@ -147,11 +147,16 @@ def Build(build_dir, arch, use_goma, extra_gn_args):
   gn_args_str = '--args=' + ' '.join([
       k + '=' + _EncodeForGN(v) for k, v in gn_args.items()] + extra_gn_args)
 
-  _RunGN(['gen', output_directory, gn_args_str])
+  gn_args = ['gen', output_directory, gn_args_str]
+  if verbose:
+    gn_args.extend(['-v'])
+  _RunGN(gn_args)
 
   ninja_args = TARGETS[:]
   if use_goma:
     ninja_args.extend(['-j', '200'])
+  if verbose:
+    ninja_args.extend(['-v'])
   _RunNinja(output_directory, ninja_args)
 
 
@@ -181,12 +186,12 @@ def GenerateLicenses(output_dir, build_dir, archs):
 
 
 def BuildAar(archs, output_file, use_goma=False, extra_gn_args=None,
-             ext_build_dir=None):
+             ext_build_dir=None, verbose=False):
   extra_gn_args = extra_gn_args or []
   build_dir = ext_build_dir if ext_build_dir else tempfile.mkdtemp()
 
   for arch in archs:
-    Build(build_dir, arch, use_goma, extra_gn_args)
+    Build(build_dir, arch, use_goma, extra_gn_args, verbose)
 
   with zipfile.ZipFile(output_file, 'w') as aar_file:
     # Architecture doesn't matter here, arbitrarily using the first one.
@@ -206,7 +211,7 @@ def main():
   logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
 
   BuildAar(args.arch, args.output, args.use_goma, args.extra_gn_args,
-           args.build_dir)
+           args.build_dir, args.verbose)
 
 
 if __name__ == '__main__':
