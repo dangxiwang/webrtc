ec2f118fc1afd36ca4df23678b226f5968539784
Jonas Olsson <jonasolsson@webrtc.org>
2018-05-31 15:21:47 +0200

    Refactor checks to use new logging backend.
    Bug: webrtc:8982
    Change-Id: I66854dd333f568d9b2a5f46bbead14b2e31179be

--- rtc_base/checks.cc
+++ rtc_base/checks.cc
@@ -24,75 +24,9 @@
 #include <windows.h>
 #endif
 
-#if defined(WEBRTC_WIN)
-#define LAST_SYSTEM_ERROR (::GetLastError())
-#elif defined(__native_client__) && __native_client__
-#define LAST_SYSTEM_ERROR (0)
-#elif defined(WEBRTC_POSIX)
-#include <errno.h>
-#define LAST_SYSTEM_ERROR (errno)
-#endif  // WEBRTC_WIN
-
 #include "rtc_base/checks.h"
 
-#if defined(_MSC_VER)
-// Warning C4722: destructor never returns, potential memory leak.
-// FatalMessage's dtor very intentionally aborts.
-#pragma warning(disable:4722)
-#endif
-
 namespace rtc {
-namespace {
-
-void VPrintError(const char* format, va_list args) {
-#if defined(WEBRTC_ANDROID)
-  __android_log_vprint(ANDROID_LOG_ERROR, RTC_LOG_TAG_ANDROID, format, args);
-#else
-  vfprintf(stderr, format, args);
-#endif
-}
-
-#if defined(__GNUC__)
-void PrintError(const char* format, ...)
-    __attribute__((__format__(__printf__, 1, 2)));
-#endif
-
-void PrintError(const char* format, ...) {
-  va_list args;
-  va_start(args, format);
-  VPrintError(format, args);
-  va_end(args);
-}
-
-}  // namespace
-
-FatalMessage::FatalMessage(const char* file, int line) {
-  Init(file, line);
-}
-
-FatalMessage::FatalMessage(const char* file, int line, std::string* result) {
-  Init(file, line);
-  stream_ << "Check failed: " << *result << std::endl << "# ";
-  delete result;
-}
-
-RTC_NORETURN FatalMessage::~FatalMessage() {
-  fflush(stdout);
-  fflush(stderr);
-  stream_ << std::endl << "#" << std::endl;
-  PrintError("%s", stream_.str().c_str());
-  fflush(stderr);
-  abort();
-}
-
-void FatalMessage::Init(const char* file, int line) {
-  stream_ << std::endl
-          << std::endl
-          << "#" << std::endl
-          << "# Fatal error in " << file << ", line " << line << std::endl
-          << "# last system error: " << LAST_SYSTEM_ERROR << std::endl
-          << "# ";
-}
 
 // MSVC doesn't like complex extern templates and DLLs.
 #if !defined(COMPILER_MSVC)
@@ -114,5 +48,6 @@ template std::string* MakeCheckOpString<std::string, std::string>(
 // Function to call from the C version of the RTC_CHECK and RTC_DCHECK macros.
 RTC_NORETURN void rtc_FatalMessage(const char* file, int line,
                                    const char* msg) {
-  rtc::FatalMessage(file, line).stream() << msg;
+  // TODO(jonasolsson) add replacement
+  abort();
 }
