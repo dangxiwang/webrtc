3a8a1cb2ab9ebc8a91fcd7d94368fa3961ed8b96
Benjamin Wright <benwright@webrtc.org>
2018-08-28 14:06:49 -0700

    This change integrates the FrameEncryptorInterface and the FrameDecryptorInterface into the public WebRTC API surface.
    This change just addresses the headers and not the internal changes.
    
    Bug: webrtc:9681
    Change-Id: I1db0172fe55ba378f62e7781c2b7dcdb93d63239

---
+++ api/crypto/framecryptoparams.cc
@@ -0,0 +1,38 @@
+
+/*
+ *  Copyright 2018 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "api/crypto/framecryptoparams.h"
+#include <string>
+
+namespace webrtc {
+
+FrameCryptoParams::FrameCryptoParams() = default;
+FrameCryptoParams::~FrameCryptoParams() = default;
+FrameCryptoParams::FrameCryptoParams(FrameCryptoParams&&) = default;
+FrameCryptoParams& FrameCryptoParams::operator=(FrameCryptoParams&&) = default;
+
+void FrameCryptoParams::SetCipherSuite(const std::string& cipher_suite) {
+  cipher_suite_ = cipher_suite;
+}
+
+std::string FrameCryptoParams::GetCipherSuite() const {
+  return cipher_suite_;
+}
+
+void FrameCryptoParams::SetKey(const rtc::ArrayView<const uint8_t> key) {
+  key_.SetData(key.data(), key.size());
+}
+
+rtc::ArrayView<const uint8_t> FrameCryptoParams::GetKey() const {
+  return rtc::ArrayView<const uint8_t>(key_.data(), key_.size());
+}
+
+}  // namespace webrtc
---
+++ api/crypto/framedecryptorinterface.h
@@ -0,0 +1,50 @@
+/*
+ *  Copyright 2018 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef API_CRYPTO_FRAMEDECRYPTORINTERFACE_H_
+#define API_CRYPTO_FRAMEDECRYPTORINTERFACE_H_
+
+#include "api/array_view.h"
+#include "api/crypto/framecryptoparams.h"
+#include "rtc_base/refcount.h"
+
+namespace webrtc {
+
+// FrameDecryptorInterface allows users to provide a custom decryption
+// implementation for all incoming frames. The user must also provide a
+// FrameEncryptorInterface to be able to encrypt the frames being sent out of
+// the device. Note this is an additional layer of encyrption in addition to the
+// standard DTLS-SRTP mechanism and is not intended to be used without it.
+// Note:
+// This interface is not ready for production use.
+class FrameDecryptorInterface : public rtc::RefCountInterface {
+ public:
+  virtual ~FrameDecryptorInterface() {}
+
+  // FrameCryptoParameters defines the keying material and cryptosuite to use
+  // when decrypting frames. The decryptor takes ownership of the parameters
+  // to prevent keying material existing in multiple locations in memory.
+  virtual void SetParameters(FrameCryptoParams params) = 0;
+
+  // Attempts to decrypt the encrypted frame. You may assume the frame size will
+  // be allocated to the size returned from GetOutputSize.
+  virtual bool Decrypt(CryptoMediaType media_type,
+                       rtc::ArrayView<const uint8_t> encrypted_frame,
+                       rtc::ArrayView<uint8_t> frame) = 0;
+
+  // Returns the total required length in bytes for the output of the
+  // decryption.
+  virtual size_t GetOutputSize(CryptoMediaType media_type,
+                               size_t encrypted_frame_size) = 0;
+};
+
+}  // namespace webrtc
+
+#endif  // API_CRYPTO_FRAMEDECRYPTORINTERFACE_H_
