626db6f8fe1a5eaea037c780c368ea3a6c48771a
Benjamin Wright <benwright@webrtc.org>
2018-08-28 15:50:20 -0700

    This change integrates the FrameEncryptorInterface and the FrameDecryptorInterface into the public WebRTC API surface.
    This change just addresses the headers and not the internal changes.
    
    Bug: webrtc:9681
    Change-Id: I1db0172fe55ba378f62e7781c2b7dcdb93d63239

---
+++ api/crypto/framecryptoparams.h
@@ -0,0 +1,44 @@
+
+/*
+ *  Copyright 2018 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef API_CRYPTO_FRAMECRYPTOPARAMS_H_
+#define API_CRYPTO_FRAMECRYPTOPARAMS_H_
+
+#include <string>
+
+#include "rtc_base/buffer.h"
+
+namespace webrtc {
+
+class FrameCryptoParams final {
+ public:
+  FrameCryptoParams();
+  ~FrameCryptoParams();
+  // This object is not copyable or assignable.
+  FrameCryptoParams(const FrameCryptoParams&) = delete;
+  FrameCryptoParams& operator=(const FrameCryptoParams&) = delete;
+  // This object is only moveable.
+  FrameCryptoParams(FrameCryptoParams&&);
+  FrameCryptoParams& operator=(FrameCryptoParams&&);
+
+  void SetCipherSuite(const std::string& cipher_suite);
+  std::string GetCipherSuite() const;
+  void SetKey(const rtc::ArrayView<const uint8_t> key);
+  rtc::ArrayView<const uint8_t> GetKey() const;
+
+ private:
+  std::string cipher_suite_;
+  rtc::ZeroOnFreeBuffer<uint8_t> key_;
+};
+
+}  // namespace webrtc
+
+#endif  // API_CRYPTO_FRAMECRYPTOPARAMS_H_
