6a574254216ac91ddb6753e8def37b4afa40ccc9
Christian Fremerey <chfremer@webrtc.org>
2018-09-18 20:52:09 +0000

    Revert "Merge system_wrappers:metrics and system_wrappers:metrics_default."
    This reverts commit c1c2a8875f6220bcb1ca543b9e8c01a1c665992d.
    
    Reason for revert: Causes linker error, see https://bugs.chromium.org/p/chromium/issues/detail?id=885205
    
    Original change's description:
    > Merge system_wrappers:metrics and system_wrappers:metrics_default.
    > 
    > After this CL, WebRTC clients will be able to exclude the default
    > metrics implementation by defining the preprocessor macro
    > WEBRTC_EXCLUDE_METRICS_DEFAULT (if GN is used, it will be enough
    > to set rtc_exclude_metrics_default to true).
    > 
    > Bug: webrtc:9631
    > Change-Id: Id6db23cc4b6c292d9f97372a8014c0c467ed0538
    > No-Try: True
    > Reviewed-on: https://webrtc-review.googlesource.com/98102
    > Commit-Queue: Mirko Bonadei <mbonadei@webrtc.org>
    > Reviewed-by: Karl Wiberg <kwiberg@webrtc.org>
    > Cr-Commit-Position: refs/heads/master@{#24747}
    
    TBR=mbonadei@webrtc.org,kwiberg@webrtc.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: webrtc:9631
    Change-Id: Ib1f2c45425a74542a69972a1be095f3fb470f6f7

--- system_wrappers/source/metrics_default.cc
+++ system_wrappers/source/metrics_default.cc
@@ -7,12 +7,13 @@
 // be found in the AUTHORS file in the root of the source tree.
 //
 
-#include "system_wrappers/include/metrics.h"
+#include "system_wrappers/include/metrics_default.h"
 
 #include <algorithm>
 
 #include "rtc_base/criticalsection.h"
 #include "rtc_base/thread_annotations.h"
+#include "system_wrappers/include/metrics.h"
 
 // Default implementation of histogram methods for WebRTC clients that do not
 // want to provide their own implementation.
@@ -202,7 +203,6 @@ RtcHistogramMap* GetMap() {
 }
 }  // namespace
 
-#ifndef WEBRTC_EXCLUDE_METRICS_DEFAULT
 // Implementation of histogram methods in
 // webrtc/system_wrappers/interface/metrics.h.
 
@@ -258,8 +258,6 @@ void HistogramAdd(Histogram* histogram_pointer, int sample) {
   RtcHistogram* ptr = reinterpret_cast<RtcHistogram*>(histogram_pointer);
   ptr->Add(sample);
 }
-
-#endif  // WEBRTC_EXCLUDE_METRICS_DEFAULT
 
 SampleInfo::SampleInfo(const std::string& name,
                        int min,
