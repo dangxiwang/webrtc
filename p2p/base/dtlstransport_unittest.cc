589a4202d2c8d886e60ef37165ddc3eaa321a59b
Zach Stein <zstein@webrtc.org>
2018-11-27 14:05:18 -0800

    Log DTLS state changes to RtcEventLog
    Bug: webrtc:9972
    Change-Id: Icb2cdda87c1c42607ed06653e053699532f25e84

--- p2p/base/dtlstransport_unittest.cc
+++ p2p/base/dtlstransport_unittest.cc
@@ -88,7 +88,7 @@ class DtlsTestClient : public sigslot::has_slots<> {
         this, &DtlsTestClient::OnFakeIceTransportReadPacket);
 
     dtls_transport_ = absl::make_unique<DtlsTransport>(
-        std::move(fake_ice_transport), webrtc::CryptoOptions());
+        std::move(fake_ice_transport), webrtc::CryptoOptions(), nullptr);
     dtls_transport_->SetSslMaxProtocolVersion(ssl_max_version_);
     // Note: Certificate may be null here if testing passthrough.
     dtls_transport_->SetLocalCertificate(certificate_);
