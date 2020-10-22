1db4c92db72336a051e0f02e4d45d3cc0a3eb8a1
Andrey Logvin <landrey@webrtc.org>
2020-10-22 13:45:51 +0000

    Add possibility of upload check for pc perf tests
    Bug:None
    Change-Id: I598d3aea8df9429bdff18b80a400c358fa1461d2

--- tools_webrtc/perf/webrtc_dashboard_upload.py
+++ tools_webrtc/perf/webrtc_dashboard_upload.py
@@ -51,6 +51,18 @@ def _CreateParser():
                       help='Where to write the output (for debugging).')
   parser.add_argument('--outdir', required=True,
                       help='Path to the local out/ dir (usually out/Default)')
+  parser.add_argument('--wait-for-upload', action='store_true',
+                      help='If specified, script will wait untill Chrome perf '
+                      'dashboard confirms that the data was succesfully '
+                      'proccessed and uploaded')
+  parser.add_argument('--wait-timeout-sec', type=int, default=600,
+                      help='Used only if wait-for-upload is True. Maximum '
+                      'amount of time in seconds that the script will wait '
+                      'for the confirmation.')
+  parser.add_argument('--wait-polling-period-sec', type=int, default=120,
+                      help='Used only if wait-for-upload is True. Status will '
+                      'be requested from the Dashboard every '
+                      'wait-polling-period-sec seconds.')
   return parser
 
 
