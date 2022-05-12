7d69fbad8b25d67577a3598b5c0df25b6794bf37
Victor Boivie <boivie@webrtc.org>
2022-05-12 22:38:51 +0200

    dcsctp: Add priority support to send queue
    This mainly modifies the stream scheduler to add a weighted fair queuing
    algorithm in addition to its round robin algorithm. The WFQ algorithm is
    selected whenever interleaving is enabled, to ensure that the socket
    stays backwards compatible in the normal (non-interleaved) scenario.
    
    Adaptation to send queue and socket comes in a follow-up CL.
    
    Bug: webrtc:5696
    Change-Id: I8f0dbfa8c2f40f2e84cee536ea821e7ef4af6310

--- net/dcsctp/tx/rr_send_queue.cc
+++ net/dcsctp/tx/rr_send_queue.cc
@@ -39,6 +39,8 @@ RRSendQueue::RRSendQueue(absl::string_view log_prefix,
     : log_prefix_(std::string(log_prefix) + "fcfs: "),
       buffer_size_(buffer_size),
       default_priority_(default_priority),
+      // TODO(webrtc:5696): Provide correct MTU.
+      scheduler_(DcSctpOptions::kMaxSafeMTUSize),
       on_buffered_amount_low_(std::move(on_buffered_amount_low)),
       total_buffered_amount_(std::move(on_total_buffered_amount_low)) {
   total_buffered_amount_.SetLowThreshold(total_buffered_amount_low_threshold);
