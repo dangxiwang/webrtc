dfc7d90ea76ae8ae28f0c9368a3285b0187ef556
Dor Hen <dorhen@meta.com>
2024-02-13 12:00:39 +0200

    Provide test output path with `OutputPathWithRandomDirectory` 1/n
    First commit in a series of commits to wire up the output path
    that includes a random directory for problematic tests that run in
    concurrent execution environments.
    
    Bug: webrtc:15833
    Change-Id: I5e5b3940007be773d77dbbfc953efa810e4e3ea9

--- test/testsupport/file_utils_unittest.cc
+++ test/testsupport/file_utils_unittest.cc
@@ -170,8 +170,8 @@ TEST_F(FileUtilsTest, GenerateTempFilename) {
 #define MAYBE_CreateDir CreateDir
 #endif
 TEST_F(FileUtilsTest, MAYBE_CreateDir) {
-  std::string directory = test::OutputPath() + "fileutils-unittest-empty-dir" +
-                          rtc::CreateRandomUuid();
+  std::string directory =
+      test::OutputPathWithRandomDirectory() + "fileutils-unittest-empty-dir";
   // Make sure it's removed if a previous test has failed:
   remove(directory.c_str());
   ASSERT_TRUE(webrtc::test::CreateDir(directory));
@@ -255,8 +255,7 @@ TEST_F(FileUtilsTest, WriteReadDeleteFilesAndDirs) {
 
   // Create an empty temporary directory for this test.
   const std::string temp_directory =
-      OutputPath() +
-      Path("TempFileUtilsTestReadDirectory" + rtc::CreateRandomUuid() + "/");
+      OutputPathWithRandomDirectory() + Path("TempFileUtilsTestReadDirectory/");
   CreateDir(temp_directory);
   EXPECT_NO_FATAL_FAILURE(CleanDir(temp_directory, &num_deleted_entries));
   EXPECT_TRUE(DirExists(temp_directory));
