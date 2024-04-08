53d94e3267d1d4d214023bfc975fc79b24110256
Wan-Teh Chang <wtc@google.com>
2024-04-08 14:12:55 -0700

    Make sure offset_x/y is even before CropAndScale()
    VideoFrameBuffer::CropAndScale() makes offset_x and offset_y even if the
    U,V planes are subsampled. This may result in the update rect being off
    by one. To prevent that, pass even offset_x and offset_y to
    VideoFrameBuffer::CropAndScale() if they would be made even under the
    hood.
    Change-Id: I4a70d460702f03dee72a5b8292cb25766c3e6aca

--- video/video_stream_encoder.cc
+++ video/video_stream_encoder.cc
@@ -1952,11 +1952,29 @@ void VideoStreamEncoder::EncodeVideoFrame(const VideoFrame& video_frame,
     VideoFrame::UpdateRect update_rect = video_frame.update_rect();
     if (crop_width_ < 4 && crop_height_ < 4) {
       // The difference is small, crop without scaling.
+      int offset_x = crop_width_ / 2;
+      int offset_y = crop_height_ / 2;
+      // Make sure offset is even so that u/v plane becomes aligned.
+      // VideoFrameBuffer::CropAndScale() may make offset_x and offset_y even.
+      // Since VideoFrameBuffer::CropAndScale() does not return the offset_x and
+      // offset_y values actually used, make offset_x and offset_y even now so
+      // that update_rect.offset_x and update_rect.offset_y will not be off by
+      // one.
+      if (video_frame.video_frame_buffer()->type() !=
+          VideoFrameBuffer::Type::kI444) {
+        offset_x = offset_x / 2 * 2;
+      }
+      if (video_frame.video_frame_buffer()->type() !=
+              VideoFrameBuffer::Type::kI444 &&
+          video_frame.video_frame_buffer()->type() !=
+              VideoFrameBuffer::Type::kI422) {
+        offset_y = offset_y / 2 * 2;
+      }
       cropped_buffer = video_frame.video_frame_buffer()->CropAndScale(
-          crop_width_ / 2, crop_height_ / 2, cropped_width, cropped_height,
-          cropped_width, cropped_height);
-      update_rect.offset_x -= crop_width_ / 2;
-      update_rect.offset_y -= crop_height_ / 2;
+          offset_x, offset_y, cropped_width, cropped_height, cropped_width,
+          cropped_height);
+      update_rect.offset_x -= offset_x;
+      update_rect.offset_y -= offset_y;
       update_rect.Intersect(
           VideoFrame::UpdateRect{0, 0, cropped_width, cropped_height});
 
